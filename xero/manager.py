f275021a2bcfacb44b110692215ed12002479cf7
Robert Wallhead <rwallhead@gmail.com>
2015-07-28 19:36:07 +1000

    Whitespace tweaks

--- xero/manager.py
+++ xero/manager.py
@@ -170,8 +170,9 @@ class Manager(object):
             headers['User-Agent'] = 'pyxero/%s ' % VERSION + requests.utils.default_user_agent()
 
             response = getattr(requests, method)(
-                    uri, data=body, headers=headers, auth=self.credentials.oauth,
-                    params=params, cert=cert, timeout=timeout)
+                uri, data=body, headers=headers, auth=self.credentials.oauth,
+                params=params, cert=cert, timeout=timeout
+            )
 
             if response.status_code == 200:
                 # If we haven't got XML or JSON, assume we're being returned a binary file
@@ -292,7 +293,7 @@ class Manager(object):
                 if key in self.BOOLEAN_FIELDS:
                     return 'true' if value else 'false'
                 elif key in self.DATE_FIELDS:
-                   return 'DateTime(%s,%s,%s)' % (value.year, value.month, value.day)
+                    return 'DateTime(%s,%s,%s)' % (value.year, value.month, value.day)
                 elif key in self.DATETIME_FIELDS:
                     return value.isoformat()
                 else:
@@ -335,8 +336,10 @@ class Manager(object):
             # Treat any remaining arguments as filter predicates
             # Xero will break if you search without a check for null in the first position:
             # http://developer.xero.com/documentation/getting-started/http-requests-and-responses/#title3
-            sortedkwargs = sorted(six.iteritems(kwargs),
-                key=lambda item: -1 if 'isnull' in item[0] else 0)
+            sortedkwargs = sorted(
+                six.iteritems(kwargs),
+                key=lambda item: -1 if 'isnull' in item[0] else 0
+            )
             for key, value in sortedkwargs:
                 filter_params.append(generate_param(key, value))
 
